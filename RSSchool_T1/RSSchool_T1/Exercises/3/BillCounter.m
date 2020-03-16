5486c93461e0e937c3916315ba5e8c8b8423112b
Julia <juliakuntsevich@gmail.com>
2020-03-16 20:17:46 +0100

    Implement BillCounter task

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,17 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    NSMutableArray *billToSplit = [NSMutableArray arrayWithArray:bill];
+    [billToSplit removeObjectAtIndex:index];
+    
+    NSNumber *totalSum = [billToSplit valueForKeyPath: @"@sum.self"];
+    
+    NSInteger difference = sum.integerValue - totalSum.integerValue / 2;
+    if (difference == 0) {
+        return @"Bon Appetit";
+    } else {
+        return [@(difference) stringValue];
+    }
 }
 
 @end
