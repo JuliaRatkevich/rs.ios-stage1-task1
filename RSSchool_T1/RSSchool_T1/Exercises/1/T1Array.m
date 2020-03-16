8f0b0c139197140e810b7426b81fc62dd2a6b85a
Julia <juliakuntsevich@gmail.com>
2020-03-16 19:30:00 +0100

    Implement T1Array task

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,26 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    NSMutableArray *happyArray = [NSMutableArray new];
+    
+    for (NSInteger i = sadArray.count - 1; i >= 0; i--) {
+        NSNumber *currentNumber = sadArray[i];
+        
+        if (i == 0 || i + 1 == sadArray.count) {
+            [happyArray addObject:currentNumber];
+            continue;
+        }
+        
+        NSNumber *previousNumber = sadArray[i - 1];
+        NSNumber *nextNumber = happyArray.lastObject;
+        
+        int sumOfNearestElements = previousNumber.intValue + nextNumber.intValue;
+        if (currentNumber.intValue <= sumOfNearestElements) {
+            [happyArray addObject:currentNumber];
+        }
+    }
+    
+    return [[[happyArray reverseObjectEnumerator] allObjects] copy];
 }
 
 @end
